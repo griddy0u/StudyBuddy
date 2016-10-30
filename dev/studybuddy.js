3a7430eec312f518a310a757e7cadbee92f289e4
Ingrid Ou <inguridoh@gmail.com>
2016-10-30 14:41:41 -0700

    Added Dev Folder

---
+++ dev/studybuddy.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+  res.sendfile('static/homepage.html');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port', process.env.PORT);
+});
