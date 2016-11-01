aad37b71074d797682293d0e1b46856790c5ae12
Ingrid Ou <inguridoh@gmail.com>
2016-11-01 15:52:06 -0700

    Changes to app js file

--- studybuddy.js
+++ studybuddy.js
@@ -1,13 +1,42 @@
+/**
+ * Module dependencies.
+ */
 var express = require('express');
 var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+// Routes
+var homepage = require('./routes/homepage');
+
 var app = express();
 
-app.use(express.static('static'));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.session());
+app.use(app.router);
 
-app.get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Print logs to the console and compress pages we send
+app.use(express.logger());
+app.use(express.compress());
+
+/*app.get('/', function (req, res) {
   res.sendfile('views/homepage.handlebars');
-});
+});*/
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port', process.env.PORT);
+// Call route file
+app.get('/', homepage.view);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
