6a3c7bac332b4cca9f8947d83ba68b5c79b7064d
Ingrid Ou <inguridoh@gmail.com>
2016-11-01 16:25:14 -0700

    Test

--- studybuddy.js
+++ studybuddy.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-app.use(express.static('views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendfile('views/homepage.html');
