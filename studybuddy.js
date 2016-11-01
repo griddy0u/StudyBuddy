496bec4c8f8a6b3479c13c433525ffe879c17fc0
Ingrid Ou <inguridoh@gmail.com>
2016-11-01 16:27:46 -0700

    Test

--- studybuddy.js
+++ studybuddy.js
@@ -2,11 +2,7 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function (req, res) {
-  res.sendfile('views/homepage.html');
-});
+app.use(express.static(__dirname + '/views'));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port', process.env.PORT);
