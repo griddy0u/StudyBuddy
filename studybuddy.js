6cc51f12f1a5613d143c5e634a82ee28f9b48a36
Ingrid Ou <inguridoh@gmail.com>
2016-11-01 16:18:09 -0700

    Revert

--- studybuddy.js
+++ studybuddy.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
-
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('static'));
 
 app.get('/', function (req, res) {
-  res.sendfile('public/homepage.handlebars');
+  res.sendfile('static/homepage.html');
 });
 
 app.listen(process.env.PORT || 3000, function () {
