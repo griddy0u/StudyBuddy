6d8fd8ca18c41b601091ef0018949c22e76cc904
Richard Oh <rioh@ucsd.edu>
2016-11-08 22:39:48 -0800

    Fixed deploy error

--- studybuddy.js
+++ studybuddy.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendfile('static/login.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000');
 });
