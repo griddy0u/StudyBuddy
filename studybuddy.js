3ae1733c71b2fcb30ea1ac2850983e979cff4455
Richard Oh <rioh@ucsd.edu>
2016-11-17 15:38:02 -0800

    Application Error fix

--- studybuddy.js
+++ studybuddy.js
@@ -9,11 +9,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static('static'));
 app.get('/', index1.view1);
 app.get('/new', index2.view2);
-/*
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port 3000');
-});
-*/
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
