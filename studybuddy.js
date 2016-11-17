d7b75c9f3fcd0eb601bcf902c89f86586f6ff9fc
Richard Oh <rioh@ucsd.edu>
2016-11-17 15:28:21 -0800

    Changes with express

--- studybuddy.js
+++ studybuddy.js
@@ -3,10 +3,17 @@ var http = require('http');
 var app = express();
 var index1 = require('./routes/index1');
 var index2 = require('./routes/index2');
+
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.static('static'));
 app.get('/', index1.view1);
 app.get('/new', index2.view2);
-
+/*
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000');
+});
+*/
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
