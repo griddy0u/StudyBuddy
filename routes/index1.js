d45a043bc8f046281cad487be598ba799b000f5f
Richard Oh <rioh@ucsd.edu>
2016-11-27 21:12:33 -0800

    Removed alternate

--- routes/index1.js
+++ routes/index1.js
@@ -2,7 +2,7 @@ var data = require('../static/data.json');
 var jsonfile = require('jsonfile');
 
 exports.view1 = function(req,res){
-  data.new = false;
+  data.new = true;
   jsonfile.writeFile('static/data.json', data, function(err){
     if(err) throw err;
   });
